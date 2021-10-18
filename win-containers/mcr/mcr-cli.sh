5fed9b4d9a85eb12230426cfec0be9d55f019b3b
Wes Higbee <wes.mcclure@gmail.com>
2021-10-18 01:38:00 -0400

    examples of mining tag data

--- win-containers/mcr/mcr-cli.sh
+++ win-containers/mcr/mcr-cli.sh
@@ -21,3 +21,54 @@ function mcr_tag_ls_txt() {
         | jq -r ".tags[]" \
         | sort
 }
+
+
+
+
+
+###################################################################
+## the rest of these are just examples
+## NOT MEANT TO BE PRECISE - just a few text mining exercises:
+###################################################################
+# greps are broken out by line to be easier to remove
+# -v = not matching
+# remove -c on `uniq -c` if you don't want count
+#
+# usage:
+# tags_for_osversions windows.nanoserver.tags.txt
+
+# known issues
+# preview tag for win10 
+# _ vs - in revision KB tags
+
+function tags_for_ltscs_all_image_txts(){
+    # searches all .tags.txt files
+    cat *.tags.txt | grep ltsc | sort | uniq
+}
+
+function tags_for_osversions(){
+  image_txt=$1
+  grep '^10\.' $image_txt \
+    | cut -d '.' -f1-3 \
+    | sort | uniq -c
+}
+
+function tags_for_builds(){
+    image_txt=$1
+    cat $image_txt \
+        | grep -v '^10\.' $image_txt \
+        | grep -v '[-_]amd64' \
+        | grep -v '[-_]arm' \
+        | grep -v '[-_]' \
+        | sort | uniq
+}
+
+function tags_for_revisions(){
+    image_txt=$1
+    cat $image_txt \
+        | grep -v '^10\.' $image_txt \
+        | grep -v '[-_]amd64' \
+        | grep -v '[-_]arm' \
+        | grep '[-_]KB' \
+        | sort | uniq
+}
