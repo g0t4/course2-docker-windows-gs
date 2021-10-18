a501e7a36893ba9388586507a91a808c9eab3826
Wes Higbee <wes.mcclure@gmail.com>
2021-10-17 20:27:53 -0400

    add iis

--- win-containers/mcr-curls.sh
+++ win-containers/mcr-curls.sh
@@ -36,4 +36,6 @@ mcr_tag_ls_txt dotnet/sdk > dotnet.sdk.tags.txt
 
 # app images
 mcr_tag_ls_txt mssql/server > mssql.server.tags.txt 
-
+# https://hub.docker.com/_/microsoft-windows-servercore-iis
+# teaser about this iis image: https://techcommunity.microsoft.com/t5/containers/contain-your-excitement-updates-on-windows-server-2022-and/ba-p/2820015
+mcr_tag_ls_txt windows/servercore/iis > iis.tags.txt 
