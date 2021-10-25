fcb2502e9b3ce0813a36ecbb8a97333d6e533fb5
Wes Higbee <wes.mcclure@gmail.com>
2021-10-25 11:14:09 -0400

    split out and refactor to an automated build script (1-build) plus set aside manual steps to verify/test (2-test) - SRP helps with automation

--- images/commits-scripted/1-build.sh
+++ images/commits-scripted/1-build.sh
@@ -1,1 +1,17 @@
 set +x
+
+# create nginx web container (simulate a running machine)
+docker container create --name web2 nginx
+
+# before
+docker container diff web2 # ≈empty + runtime state if testing
+# replace page
+docker container cp index.html web2:/usr/share/nginx/html/index.html
+# after
+docker container diff web2 # + custom page
+
+
+docker image ls # before
+# create image
+docker container commit web2 nginx-commits2
+docker image ls # after (+ nginx-commits image)
--- images/commits-scripted/2-test.sh
+++ images/commits-scripted/2-test.sh
@@ -1,1 +1,6 @@
 set +x
+
+# testing afterwards
+curl localhost:8081 # before (site offline)
+docker container run -i -t -p 8081:80 --name web-commits2 nginx-commits2
+curl localhost:8081 # after
