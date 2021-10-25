0d411009544223cabd40437ee712ca5a4ff84524
Wes Higbee <wes.mcclure@gmail.com>
2021-10-24 22:36:16 -0400

    more tools and make shell script

--- wes-wsl-distro/windows-terminal/packages.sh
+++ wes-wsl-distro/windows-terminal/packages.sh
@@ -1,4 +1,15 @@
 
+
+# YMMV - additional tools I used in demos or in prep
+sudo apt-get install -y \
+  bat \
+  jq \
+  tree \
+  silversearcher-ag \
+  fzf \
+  tldr \
+  zsh
+
 # dive - image diffs
 wget https://github.com/wagoodman/dive/releases/download/v0.10.0/dive_0.10.0_linux_amd64.deb
 sudo apt install ./dive_0.10.0_linux_amd64.deb
