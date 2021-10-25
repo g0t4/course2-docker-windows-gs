6945e77471c93d111aca36515a9f9a65eb572aff
Wes Higbee <wes.mcclure@gmail.com>
2021-10-25 11:34:52 -0400

    off screen add in changes to speed up testing which is less important

--- images/commits-scripted/2-test.sh
+++ images/commits-scripted/2-test.sh
@@ -1,6 +1,10 @@
 set -x
 
 # testing afterwards
-curl localhost:8081 # before (site offline)
-docker container run -i -t -p 8081:80 --name web-commits2 nginx-commits2
-curl localhost:8081 # after
+curl localhost:8082 # before (site offline)
+docker container run -d -p 8082:80 --name web-commits2 nginx-commits2
+sleep 3s
+curl localhost:8082 # after
+
+docker container stop web-commits2
+docker container rm web-commits2
