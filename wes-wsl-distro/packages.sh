7c61d9fc3f3c83300b7c9c551c5cf7ecf3e37e38
Wes Higbee <wes.mcclure@gmail.com>
2021-10-26 18:41:30 -0700

    swap out apt for apt-get

--- wes-wsl-distro/packages.sh
+++ wes-wsl-distro/packages.sh
@@ -10,8 +10,9 @@ sudo apt-get install -y \
   tldr \
   zsh
 
+
 # dive - image diffs
 DIVE_VERSION="0.10.0"
 wget https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
-sudo apt install -y ./dive_${DIVE_VERSION}_linux_amd64.deb
+sudo apt-get install -y ./dive_${DIVE_VERSION}_linux_amd64.deb
 # rm ./dive_${DIVE_VERSION}_linux_amd64.deb
