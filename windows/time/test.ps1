c609fecc8360d55e3af5b2ca07e207b123f0a7db
Wes Higbee <wes.mcclure@gmail.com>
2021-11-09 21:44:30 -0500

    not enough time to cover all

--- windows/time/test.ps1
+++ windows/time/test.ps1
@@ -11,17 +11,12 @@ docker build --tag set-tz:nanoserver `
 docker build --tag set-tz:servercore `
   --file Dockerfile.set-tz-servercore .
 
-# take 2
-docker build --tag set-tz:nanoserver `
-  --file Dockerfile.tzutil-nanoserver .
-
 # check again
 hostname; (get-timezone).DisplayName; (get-date).ToShortTimeString();
 
 # run examples
 docker container run --rm -it no-set-tz
 docker container run --rm -it set-tz:servercore
-docker container run --rm -it set-tz:nanoserver
 
 # check again
 hostname; (get-timezone).DisplayName; (get-date).ToShortTimeString();
