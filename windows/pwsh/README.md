f3b393022a74615d17a493679631d25cd90fd10e
Wes Higbee <wes.mcclure@gmail.com>
2021-11-20 00:28:02 -0500

    split apart profile sample from instructions to set it up

---
+++ windows/pwsh/README.md
@@ -0,0 +1,7 @@
+- docker CLI COMPLETIONS
+  - [docker/cli repo - powershell completions](https://github.com/docker/cli/tree/master/contrib/completion/powershell)
+    - links to [DockerCompletion package](https://github.com/matt9ucci/DockerCompletion)
+    - TLDR: `Install-Package DockerCompletions`
+- Now, setup a profile to load this and other options by default every time you open a powershell session
+  - `code $PROFILE`
+  - use provided contents of [profile.ps1](./profile.ps1)
--- windows/pwsh/profile.ps1
+++ windows/pwsh/profile.ps1
@@ -1,21 +1,11 @@
-# create a file $PROFILE
-# ie use vscode:
-code $PROFILE 
+# the following is a sample profile that loads every time you start up an interactive powershell session
 
-
-## remainder is example of powershell profile:
-# put the remainder of this file into the $PROFILE file
-# these are just things discussed in the course
-
-# Add docker tab completion to PowerShell
-# make sure to install package (module) first: (one time install)
-# - Install-Package DockerCompletions
 Import-Module DockerCompletion
 
 # Trigger menu completion with tab key (override default tab complete)
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 
 # Change up/down behavior so it doesn't scroll through all lines
-# and instead matches on what you already typed in the current line 
+# and instead matches on what you already typed in the current line
 Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
